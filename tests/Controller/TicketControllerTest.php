a8d45dd43f27d663b17a3befbd8a94b214d9825a
Julien <martin-julien@outlook.fr>
2018-11-25 01:50:57 +0100

    Unit test and func test

--- tests/Controller/TicketControllerTest.php
+++ tests/Controller/TicketControllerTest.php
@@ -12,4 +12,39 @@ class TicketControllerTest extends WebTestCase {
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
+    public function testTicket(){
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/billeterie');
+
+        $form = $crawler->selectButton('Valider')->form();
+        $form['reservation[email]'] = 'test@test.fr';
+        $form['reservation[visitDate]'] = \DateTime::class;
+        if(date('H') >= 14){
+            $form['reservation[halfDay]'] = true;
+        }
+
+        $values = array_merge_recursive(
+            $form->getPhpValues(),
+            array(
+                'reservation[visitors]' => array (
+                    'visitors' => array(
+                        array(
+                            'reservation[visitors][0][lastname]' => 'Martin',
+                            'reservation[visitors][0][firstname]' => 'Julien',
+                            'reservation[visitors][0][birthdate][day]' => '15',
+                            'reservation[visitors][0][birthdate][month]' => '01',
+                            'reservation[visitors][0][birthdate][year]' => '1996',
+                            'reservation[visitors][0][nationality]' => 'Française',
+                            'reservation[visitors][0][reducedRate]' => 1
+                        )
+                    )
+                )
+            )
+        );
+
+        $client->request($form->getMethod(), $form->getUri(), $values, $form->getPhpFiles());
+        $client->submit($form);
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+    }
+
 }
